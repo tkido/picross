51357d86563e98ec62122d18a10488299427899d
tkido <takanorikido@gmail.com>
2015-08-04 15:12:47 +0900

    Add main, Config

---
+++ src/com/tkido/picross/main.scala
@@ -0,0 +1,11 @@
+package com.tkido.picross
+
+object main extends App {
+  import com.tkido.picross.Config
+  import com.tkido.tools.Logger
+  import com.tkido.tools.Text
+  
+  Logger.level = Config.logLevel
+
+  Logger.close()
+}
